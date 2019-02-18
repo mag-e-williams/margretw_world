f62ba947489e79a68466f29288da28afa0190e6a
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-02-17 20:36:52 -0500

    added relationship to Category model and added routes for articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,6 @@ class Category < ApplicationRecord
 	has_many :photos
 	has_many :articles
 
-	validates_presence_of :title
-	validates_presence_of :content
-
 	scope :active, 			 -> { where('active = ?', true) }
   	scope :alphabetical, -> { order('name') }
   
