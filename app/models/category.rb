a5ce97b23821d615734bbe75a694482d603d41f0
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-02-17 20:12:52 -0500

    added new Article model and wrote in model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,11 @@
 class Category < ApplicationRecord
 	has_many :photos
+	has_many :articles
+
+	validates_presence_of :title
+	validates_presence_of :content
 
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+  	scope :alphabetical, -> { order('name') }
   
 end
